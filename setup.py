829aba5ac31ef5e841404c2c74a906898cec0bbb
Ben Mussay <bengordoncshaifa@gmail.com>
2019-09-27 18:37:24 +0300

    update the whl

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 import setuptools
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='Coresets-Tests',
-     version='0.1',
-     author="Ben Mussay",
-     author_email="bengordoncshaifa@gmail.com",
-     description="Test a coreset using backpropagation",
-     url="https://github.com/BenMussay/Coresets-Tests",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "Operating System :: OS Independent",
-     ],
- )
+    name="Coresets-Tests",
+    version="0.0.1",
+    author="Ben Mussay",
+    author_email="bengordoncshaifa@gmail.com",
+    description="A method for testing coresets using backpropagation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BenMussay/Coresets-Tests",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
